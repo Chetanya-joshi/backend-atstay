3f79761da4cfb4a0ae446f5df039f9ca87642b03
Chetanya-joshi <108217245+Chetanya-joshi@users.noreply.github.com>
2024-03-12 22:34:46 +0530

    Update Razorpay.js

--- Razorpay.js
+++ Razorpay.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const Razorpay = require("razorpay");
 const instance = new Razorpay({
-  key_id: process.env.RAZOR_AP1_KEY,
-  key_secret: process.env.RAZORPAY_API_SECRET,
+  key_id: 'rzp_test_OmCfFJhnp3Fztn',
+  key_secret: 'wyTuLIkM1pDzjPnYg9E3NV6E',
 });
 module.exports = instance;
